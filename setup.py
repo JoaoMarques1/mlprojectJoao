3be76101dba2860b353c1f6be7f31ffafec70c64
Joao Marques <joaofmarques1@hotmail.com>
2022-02-21 14:32:57 +0000

    Initial Push

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='mlprojectJoao',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/mlprojectJoao-run'],
+      scripts=['scripts/mlprojectJoao-run', 'scripts/mlprojectJoao-tryme'],
       zip_safe=False)
